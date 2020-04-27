cfe0163d48caba0b3da5c5234051345bff0d86ae
Peter Grimm <grimmpd@mail.uc.edu>
2020-04-27 16:28:25 -0600

    a

--- example2.py
+++ example2.py
@@ -7,13 +7,13 @@ dt_inputs = 1
 datSrc = marketData.marketData("spy")
 (inputData,labelData) = datSrc.parsePriceData(inputs,outputs,dt_inputs,dt_outputs)
 master = masterProcess(inputs,outputs)
-initPopulation = 10
+initPopulation = 20
 cutoff = .9
 c1 = .6
 c2 = .3
 c3 = .1
 epochs = 10
-learningRate = .2
+learningRate = .02
 maxGens = 10
 master.set_trainingData(inputData, labelData)
-master.evolveFeedbackParallel(initPopulation,c1,c2,c3,cutoff,maxGens,epochs,learningRate,.2,.005,.4,.6)
+master.evolveFeedbackParallel(initPopulation,c1,c2,c3,cutoff,maxGens,epochs,learningRate,.2,.005,.4,.5)
