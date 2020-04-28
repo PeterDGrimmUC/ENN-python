0d3851f37caa6bf9089a5062f602a1c508345b53
Peter Grimm <grimmpd@mail.uc.edu>
2020-04-28 16:16:29 -0600

    a

--- handwriting-test.py
+++ handwriting-test.py
@@ -1,13 +1,12 @@
 from ENN.EXAMMV2 import *
 import numpy as np
-
 image_size = 28 # width and length
 no_of_different_labels = 10 #  i.e. 0, 1, 2, 3, ..., 9
 image_pixels = image_size * image_size
 data_path = "/handwritingData/"
-train_data = np.loadtxt(data_path + "mnist_train.csv",
+train_data = np.loadtxt("mnist_train.csv",
                         delimiter=",")
-test_data = np.loadtxt(data_path + "mnist_test.csv",
+test_data = np.loadtxt("mnist_test.csv",
                        delimiter=",")
 
 def digToLabel(dig):
